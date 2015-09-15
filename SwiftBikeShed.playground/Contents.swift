489ad69b87d72f0ab4cfb8a926c5de653253ab62
Kristina Sontag <superpuppy@gmail.com>
2015-09-14 23:00:58 -0700

    Updating playground with start of recursive chop

--- SwiftBikeShed.playground/Contents.swift
+++ SwiftBikeShed.playground/Contents.swift
@@ -2,39 +2,81 @@
 
 import Cocoa
 
-// http://codekata.com/kata/kata02-karate-chop/
-/*
-def test_chop
-assert_equal(-1, chop(3, []))
-assert_equal(-1, chop(3, [1]))
-assert_equal(0,  chop(1, [1]))
-#
-assert_equal(0,  chop(1, [1, 3, 5]))
-assert_equal(1,  chop(3, [1, 3, 5]))
-assert_equal(2,  chop(5, [1, 3, 5]))
-assert_equal(-1, chop(0, [1, 3, 5]))
-assert_equal(-1, chop(2, [1, 3, 5]))
-assert_equal(-1, chop(4, [1, 3, 5]))
-assert_equal(-1, chop(6, [1, 3, 5]))
-#
-assert_equal(0,  chop(1, [1, 3, 5, 7]))
-assert_equal(1,  chop(3, [1, 3, 5, 7]))
-assert_equal(2,  chop(5, [1, 3, 5, 7]))
-assert_equal(3,  chop(7, [1, 3, 5, 7]))
-assert_equal(-1, chop(0, [1, 3, 5, 7]))
-assert_equal(-1, chop(2, [1, 3, 5, 7]))
-assert_equal(-1, chop(4, [1, 3, 5, 7]))
-assert_equal(-1, chop(6, [1, 3, 5, 7]))
-assert_equal(-1, chop(8, [1, 3, 5, 7]))
-end
+/* http://codekata.com/kata/kata02-karate-chop/
+*  Specification:
+*  Write a binary chop method that takes an integer search target and a sorted array of integers. It should return the integer index of the target in the array,
+*  or -1 if the target is not in the array. The signature will logically be:
+*      chop(int, array_of_int)  -> int
+*  You can assume that the array has less than 100,000 elements. For the purposes of this Kata, time and memory performance are not issues (assuming the chop
+*  terminates before you get bored and kill it, and that you have enough RAM to run it).
+*
+*  Basic pseudocode from Khan Academy
+*  1) Let min = 0 and max = n-1
+*  2) If max < min, then stop: target is not present in array. Return -1.
+*  3) Compute guess as the average of max and min, rounded down (so that it is an integer).
+*  4) If array[guess] equals target, then stop. You found it! Return guess.
+*  5) If the guess was too low, that is, array[guess] < target, then set min = guess + 1.  //<-- these next two are basically trimming the array we're handing on to the next iteration
+*  6) Otherwise, the guess was too high. Set max = guess - 1.
+*  7) Go back to step 2. //<-- screams recursion!
 */
 
-// Accept array (sort?)
-// Find middle number (even number of elements?)
-// Is number less than, greater than or equal to middle number? (if equal, return it and GTFO)
-// Slice array in two
-// If number lower than middle, call function with lower array slice, if greater, call function again with greater slice
+/*
+https://twitter.com/gregtitus/status/643652546293174272
+Guess #1 you may be looking for:
 
-func chop() {
-    
+extension ArraySlice where Element : Comparable {
 }
+
+Guess #2 self.startIndex self.endIndex?
+*/
+
+extension ArraySlice where Element : Comparable {
+    // do something here?
+}
+
+func recursiveBinaryChop(target: Int, numbers: [Int]) -> Int
+{
+    var max = numbers.count - 1
+    var min = 0
+    var guess = Int(floor(Double((max + min) / 2)))
+    
+    while (max >= min)
+    {
+        guess = Int(floor(Double((max + min) / 2)))
+        if (numbers[guess] == target) {
+            return guess
+        } else if (numbers[guess] > target) {
+            max = guess - 1
+            recursiveBinaryChop(target, numbers: numbers[(min..<max)])
+        } else {
+            min = guess + 1
+        }
+    }
+    return -1
+}
+
+/*
+assert(recursiveBinaryChop(3, numbers: []) == -1)
+assert(recursiveBinaryChop(3, numbers: [1]) == -1)
+assert(recursiveBinaryChop(1, numbers: [1]) == 0)
+*/
+
+//assert(recursiveBinaryChop(1, numbers: [1, 3, 5]) == 0)
+//assert(recursiveBinaryChop(3, numbers: [1, 3, 5]) == 1)
+//assert(recursiveBinaryChop(5, numbers: [1, 3, 5]) == 2)
+/*
+assert(recursiveBinaryChop(0, numbers: [1, 3, 5]) == -1)
+assert(recursiveBinaryChop(2, numbers: [1, 3, 5]) == -1)
+assert(recursiveBinaryChop(4, numbers: [1, 3, 5]) == -1)
+assert(recursiveBinaryChop(6, numbers: [1, 3, 5]) == -1)
+
+assert(recursiveBinaryChop(1, numbers: [1, 3, 5, 7]) == 0)
+assert(recursiveBinaryChop(3, numbers: [1, 3, 5, 7]) == 1)
+assert(recursiveBinaryChop(5, numbers: [1, 3, 5, 7]) == 2)
+assert(recursiveBinaryChop(7, numbers: [1, 3, 5, 7]) == 3)
+assert(recursiveBinaryChop(0, numbers: [1, 3, 5, 7]) == -1)
+assert(recursiveBinaryChop(2, numbers: [1, 3, 5, 7]) == -1)
+assert(recursiveBinaryChop(4, numbers: [1, 3, 5, 7]) == -1)
+assert(recursiveBinaryChop(6, numbers: [1, 3, 5, 7]) == -1)
+assert(recursiveBinaryChop(8, numbers: [1, 3, 5, 7]) == -1)
+*/
